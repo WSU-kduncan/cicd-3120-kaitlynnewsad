62d1a0acbeb7309f2bd28dd301842ffe331e8135
kaitlynnewsad <56359938+kaitlynnewsad@users.noreply.github.com>
2022-04-11 13:54:28 -0400

    Create pull-restart.sh

---
+++ webnook/pull-restart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# kill any current bird website containers then delete any unused images and containers
+docker kill static-nginx
+yes | docker system prune
+#Get updated image
+docker pull kaitlynnewsad/project-5:latest
+#Create container from image
+docker run -d --rm --name static-nginx -p 80:80 kaitlynnewsad/project-5:latest
