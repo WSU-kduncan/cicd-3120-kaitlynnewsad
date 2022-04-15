ec096e27a26b80d591b44010f2a63c3660144fb2
kaitlynnewsad <56359938+kaitlynnewsad@users.noreply.github.com>
2022-04-15 13:07:38 -0400

    Update pull-restart.sh

--- webnook/pull-restart.sh
+++ webnook/pull-restart.sh
@@ -6,4 +6,4 @@ yes | docker system prune
 #Get updated image
 docker pull kaitlynnewsad/project-5:latest
 #Create container from image
-docker run -d --rm --name static-nginx -p 80:80 kaitlynnewsad/project-5:latest
+docker run -d --name static-nginx -p 80:80 kaitlynnewsad/project-5:latest
